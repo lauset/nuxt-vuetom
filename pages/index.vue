ea82090877d06ee9cf8f6af0f0a51363d094f640
lauset <liujie_783@163.com>
2022-09-07 20:48:00 +0800

    move file

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,19 @@
 <template>
-   <Tutorial/>
+  <div>
+    <span @click="q">Query</span>
+    data: {{data}}
+    <Tutorial/>
+  </div>
 </template>
 <script setup>
+import { ref } from 'vue'
 definePageMeta({
   layout: 'default',
 })
+const data = ref()
+const q = async () => {
+  let res = await $fetch('/api/hello');
+  console.log('res: ', res);
+  data.value = res
+}
 </script>
